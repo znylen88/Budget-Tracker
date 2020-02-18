d3f897f2195d752a6243fdc27d4613cff7175c0d
Zachary Nylen <znylen@aol.com>
2020-02-18 15:56:36 -0800

    Update Files

--- public/db.js
+++ public/db.js
@@ -1,1 +1,54 @@
-// your code goes here
+let db;
+
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function (event) {
+    const db = event.target.result;
+    db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
+
+request.onerror = function (errorEvent) {
+    console.log("Error loading database. " + errorEvent.target.errorCode);
+};
+
+function saveRecord(record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+
+    store.add(record);
+}
+
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+                .then(response => response.json())
+                .then(() => {
+                    // Delete records if successful
+                    const transaction = db.transaction(["pending"], "readwrite");
+                    const store = transaction.objectStore("pending");
+                    store.clear();
+                });
+        }
+    };
+}
+
--- public/index.js
+++ public/index.js
@@ -1,1 +1,102 @@
-// your code goes here
+let transactions = [];
+let myChart;
+
+fetch("/api/transaction")
+    .then(response => response.json())
+    .then(data => {
+        // Save the Database
+        transactions = data;
+        renderTotal();
+        renderTable();
+    });
+
+function renderTotal() {
+    // Creates budget total
+    let total = transactions.reduce((total, t) => {
+        return total + parseInt(t.value);
+    }, 0);
+
+    let transactionsTotal = document.querySelector("#total");
+    transactionsTotal.textContent = total;
+
+    // Current budget total
+    console.log(total)
+}
+
+function renderTable() {
+    const tbody = document.querySelector("#tbody");
+    tbody.innerHTML = "";
+
+    transactions.forEach(transaction => {
+        // Create table row (name and amount) 
+        const transactionColumn = document.createElement("tr");
+        transactionColumn.innerHTML = `
+            <td>${transaction.name}</td>
+            <td>${transaction.value}</td>
+        `;
+        tbody.appendChild(transactionColumn);
+        console.log("Created New Transaction! Name: " + transaction.name + " | " + "Amount: " + transaction.value)
+    });
+}
+
+function createTransaction(addingFunds) {
+    const transactionName = document.querySelector("#t-name");
+    const transactionAmount = document.querySelector("#t-amount");
+
+    // Creates the record
+    const transaction = {
+        name: transactionName.value,
+        value: transactionAmount.value,
+        date: new Date().toISOString()
+    };
+
+    // If subtracting, convert value to negative number
+    if (!addingFunds) {
+        transaction.value *= -1;
+    }
+
+    // Add to beginning of array
+    transactions.unshift(transaction);
+
+    // Re-call functions to update newly created data
+    renderTable();
+    renderTotal();
+
+    // Resend data to server
+    fetch("/api/transaction", {
+        method: "POST",
+        body: JSON.stringify(transaction),
+        headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json"
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+                // Clearing text inputs
+                transactionName.value = "";
+                transactionAmount.value = "";
+            }
+        )
+        .catch(err => {
+            // Save record to indexedDB
+            saveRecord(transaction);
+
+            // Clearing text inputs
+            transactionName.value = "";
+            transactionAmount.value = "";
+        });
+}
+
+document.querySelector("#add-btn").addEventListener("click", function (event) {
+    event.preventDefault();
+    createTransaction(true);
+});
+
+document.querySelector("#sub-btn").addEventListener("click", function (event) {
+    event.preventDefault();
+    createTransaction(false);
+});
+
+// function renderChart() {
+// NOT WORKING!
